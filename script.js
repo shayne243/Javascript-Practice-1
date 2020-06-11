d1a03c73c982362445df64b4a94d1242a71e09fd
shayne243 <marques0@purdue.edu>
2020-06-10 21:13:58 -0400

    Input complete

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ function computerPlay() {
     num = Math.floor(Math.random() * (arr.length + 1));
     return arr[num];
 }
+var playerSelection = prompt("What is your choice?");
+var computerSelection = computerPlay();
